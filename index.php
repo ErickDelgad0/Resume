3b17819206f8cfb9d194d44b2d48e5b858b8d347
Erick Delgado <edelgado24@wooster.edu>
2024-05-16 17:05:47 -0400

    new try

--- index.php
+++ index.php
@@ -1,26 +1,2 @@
 <?php
-$request = $_SERVER['REQUEST_URI'];
-$viewDir = '/view/';
-
-switch ($request) {
-    case '':
-    case '/':
-        require __DIR__ . $viewDir . 'index.php';
-        break;
-    case 'views/freedom':
-        require __DIR__ . $viewDir . 'freedom.php';
-        break;
-    case 'views/projects':
-        require __DIR__ . $viewDir . 'projects.php';
-        break;
-    case 'views/starpoint-thesis':
-        require __DIR__ . $viewDir . 'starpoint-thesis.php';
-        break;
-    case 'views/starpoint':
-        require __DIR__ . $viewDir . 'starpoint.php';
-        break;
-    case 'views/sunchase':
-        require __DIR__ . $viewDir . 'sunchase.php';
-        break;
-    
-}
+echo "hello";
