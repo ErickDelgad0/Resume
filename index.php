a5a0e52635b4bf67d168154818711c238a34b70a
Erick Delgado <edelgado24@wooster.edu>
2024-05-16 17:01:43 -0400

    changes

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
-// index.php
-header("Location: ./view/index.php");
-exit();
+$request = $_SERVER['REQUEST_URI'];
+$viewDir = '/view/';
+
+switch ($request) {
+    case '':
+    case '/':
+        require __DIR__ . $viewDir . 'index.php';
+        break;
+    case 'views/freedom':
+        require __DIR__ . $viewDir . 'freedom.php';
+        break;
+    case 'views/projects':
+        require __DIR__ . $viewDir . 'projects.php';
+        break;
+    case 'views/starpoint-thesis':
+        require __DIR__ . $viewDir . 'starpoint-thesis.php';
+        break;
+    case 'views/starpoint':
+        require __DIR__ . $viewDir . 'starpoint.php';
+        break;
+    case 'views/sunchase':
+        require __DIR__ . $viewDir . 'sunchase.php';
+        break;
+    
+}
